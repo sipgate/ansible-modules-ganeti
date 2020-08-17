ac50f5700c644ba928b6af0eea6cbfc2799fa89d
Julian Geywitz <geywitz@sipgate.de>
2020-08-17 09:27:03 +0200

    Test: startup and shutdown instance

--- tests/test_ganeti_instance.py
+++ tests/test_ganeti_instance.py
@@ -1,26 +1,41 @@
+from pytest import raises
 
-def set_module_args(args):
-    import json
-    from ansible.module_utils import basic
-    from ansible.module_utils._text import to_bytes
+from conftest import set_module_args, AnsibleExitJson, AnsibleFailJson
 
-    args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
-    basic._ANSIBLE_ARGS = to_bytes(args)
-
-def test_dummy():
-    assert True
 
 def test_import_module_works():
     import ganeti_instance
 
-def test_dummy_instance_is_present():
+
+def test_dummy_instance_can_be_started(module_args, rapi_client):
     import ganeti_instance
 
     set_module_args({
-        'user': 'gnt-cc',
-        'gnt-cc': 'gnt-cc',
-        'name': 'homer',
-        'state': 'started'
+        **module_args,
+        "state": "started"
     })
 
-    ganeti_instance.main()
+    with raises(AnsibleExitJson) as result:
+        ganeti_instance.main()
+
+    assert (result.value.args[0]['changed'] is True)
+    assert (result.value.args[0]['reboot_required'] is False)
+    assert (result.value.args[0]['message'] == "startup complete")
+    assert (rapi_client.GetInstance("homer")["status"] == "running")
+
+
+def test_dummy_instance_can_be_stopped(module_args, rapi_client):
+    import ganeti_instance
+
+    set_module_args({
+        **module_args,
+        "state": "stopped"
+    })
+
+    with raises(AnsibleExitJson) as result:
+        ganeti_instance.main()
+
+    assert (result.value.args[0]['changed'] is True)
+    assert (result.value.args[0]['reboot_required'] is False)
+    assert (result.value.args[0]['message'] == "shutdown complete")
+    assert (rapi_client.GetInstance("homer")["status"] == "ADMIN_down")
