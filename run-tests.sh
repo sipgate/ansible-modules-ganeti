26478411f48518ce8740f355535e2b9a24094293
Rudolph Bott <bott@sipgate.de>
2025-08-20 17:32:05 +0200

    BEAR-760: cleanup vcluster container after successful test run

--- run-tests.sh
+++ run-tests.sh
@@ -10,8 +10,8 @@ fi
 echo "* Pulling latest ganeti-docker image"
 docker pull ghcr.io/sipgate/ganeti-docker:latest
 
-echo "* Starting ganeti-docker image in detached mode"
-docker run -d -p 5080:5080 --cap-add=NET_ADMIN ghcr.io/sipgate/ganeti-docker:latest
+echo "* Starting ganeti-docker container in detached mode"
+docker run --name ganeti_vcluster --detach --publish 5080:5080 --cap-add=NET_ADMIN ghcr.io/sipgate/ganeti-docker:latest
 
 echo "* Waiting for vcluster setup to finish (max. 60 seconds)"
 n=0; until [ "$n" -ge 60 ]; do curl --output /dev/null --silent --fail --insecure https://localhost:5080/2/info && break; n=$((n+1)); sleep 5; done
@@ -22,3 +22,6 @@ echo
 
 echo "* Executing Python tests"
 pytest -vv
+
+echo "* Stop ganeti-docker container"
+docker stop ganeti_vcluster
