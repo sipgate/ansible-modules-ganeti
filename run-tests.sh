17de43ef06e673eb1f36215eef14a1814e696224
Rudolph Bott <bott@sipgate.de>
2025-08-20 18:06:52 +0200

    BEAR-760: remove vcluster container after use

--- run-tests.sh
+++ run-tests.sh
@@ -11,7 +11,7 @@ echo "* Pulling latest ganeti-docker image"
 docker pull ghcr.io/sipgate/ganeti-docker:latest
 
 echo "* Starting ganeti-docker container in detached mode"
-docker run --name ganeti_vcluster --detach --publish 5080:5080 --cap-add=NET_ADMIN ghcr.io/sipgate/ganeti-docker:latest
+docker run --rm --name ganeti_vcluster --detach --publish 5080:5080 --cap-add=NET_ADMIN ghcr.io/sipgate/ganeti-docker:latest
 
 echo "* Waiting for vcluster setup to finish (max. 60 seconds)"
 n=0; until [ "$n" -ge 60 ]; do curl --output /dev/null --silent --fail --insecure https://localhost:5080/2/info && break; n=$((n+1)); sleep 5; done
